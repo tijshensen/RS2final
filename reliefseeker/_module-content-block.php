2bcbdb08dda5e4652ed1cf799bafb65705fcb10e
Tijs <w@tijshensen.nl>
2020-09-10 15:19:53 +0200

    Table & icon size in provider card
    Can you find out where this CSS is coming from?
    
    .rs-card-logo {
        height: 5pc;
        margin-bottom: 10px;
    }
    
    This can be removed.

--- reliefseeker/_module-content-block.php
+++ reliefseeker/_module-content-block.php
@@ -85,7 +85,373 @@
 					<p><span >There are many legalities that go along with the world of telemedicine and each state has different rules and regulations that they must follow. One of the main laws is the medical world is HIPAA (Health Insurance Portability and Accountability Act). This law serves to protect the privacy of your medical information. HIPAA encompasses many rules and regulations that give direction on the administration and enforcement of the law. The two main rules are the privacy and security rule. And just as they sound they protect the privacy of patient medical information and make sure this information is secure. The second law is a BAA (Business Association Agreement). Every healthcare organization is required through HIPAA to maintain a BAA with its technology company. Most healthcare systems utilize a third party technology group to store medical information. HIPAA does not apply to telemedicine, it is up to the providers to make sure they are being compliant with HIPAA laws. Telemedicine companies that need to utilize a third party to store their data should have a signed BAA. If they do not have a signed BAA they are confident in their ability to keep protected health information secure.</span></p>
 					<p><span >Telehealth is a new and rapidly changing idea that requires many regulations in regard to physician practice and reimbursement. There are parity laws in place that require private insurance companies to reimburse televisits the same way they would for an in-office visit. These laws can vary state by state, currently, 33 of the states comply with parity laws. Along with parity laws, many states also require patient consent for telemedicine visits. Providers can go unpaid if they fail to receive patient consent for treatment. Lastly, there are regulations on medical licensing for physicians by state. Many providers are only licensed to practice in one state. Interstate medical licensing would allow for a provider to practice across state borders. There are currently only a few states that allow interstate medical licensing.</span></p>
 					<p></p>
-									</div>
+					</div>
+					
+					<div class="full-table">
+					<div class="table-responsive-lg">
+						<table class="table table-striped table-bordered table-sm">
+						<tbody>
+						<tr>
+						<td >
+						<p><strong>Online Therapy Service</strong></p>
+						</td>
+						<td >
+						<p><strong>Best Online Therapy for</strong></p>
+						</td>
+						<td >
+						<p><strong>Therapy Cost</strong></p>
+						</td>
+						<td >
+						<p><strong>Communication Methods</strong></p>
+						</td>
+						<td >
+						<p><strong>Therapist Placement Method</strong></p>
+						</td>
+						<td >
+						<p><strong>Therapists / Counselors Available</strong></p>
+						</td>
+						<td >
+						<p><strong>Online Therapy Rating</strong></p>
+						</td>
+						<td >
+						<p><strong>Link to their Site</strong></p>
+						</td>
+						</tr>
+						<tr>
+						<td >
+						<p><strong>BetterHelp</strong></p>
+						</td>
+						<td >
+						<p>Overall</p>
+						</td>
+						<td >
+						<p>$140-$280/month</p>
+						</td>
+						<td >
+						<ul>
+						<li>Messaging</li>
+						<li>Live Chat</li>
+						<li>Phone Call</li>
+						<li>Live Video</li>
+						</ul>
+						</td>
+						<td >
+						<p>Matching</p>
+						</td>
+						<td >
+						<p>7,525</p>
+						</td>
+						<td >
+						<p>9.5/10</p>
+						</td>
+						<td >
+						<p><span><a href="https://www.betterhelp.com/">Link</a></span></p>
+						</td>
+						</tr>
+						<tr>
+						<td >
+						<p><strong>Talkspace</strong></p>
+						</td>
+						<td >
+						<p>Businesses</p>
+						</td>
+						<td >
+						<p>$260-$396/month</p>
+						</td>
+						<td >
+						<ul>
+						<li>Messaging</li>
+						<li>Live Video</li>
+						</ul>
+						</td>
+						<td >
+						<p>Matching</p>
+						</td>
+						<td >
+						<p>3,000+</p>
+						</td>
+						<td >
+						<p>9.1/10</p>
+						</td>
+						<td >
+						<p><span><a href="https://www.talkspace.com/">Link</a></span></p>
+						</td>
+						</tr>
+						<tr>
+						<td >
+						<p><strong>7 Cups</strong></p>
+						</td>
+						<td >
+						<p>Free Support</p>
+						</td>
+						<td >
+						<p>$0-$150/month</p>
+						</td>
+						<td >
+						<ul>
+						<li>Messaging</li>
+						<li>Live Chat</li>
+						<li>Phone Call</li>
+						<li>Live Video</li>
+						</ul>
+						</td>
+						<td >
+						<p>Matching</p>
+						</td>
+						<td >
+						<p>116</p>
+						</td>
+						<td >
+						<p>8.8/10</p>
+						</td>
+						<td >
+						<p><span><a href="https://www.7cups.com/">Link</a></span></p>
+						</td>
+						</tr>
+						<tr>
+						<td >
+						<p><strong>ReGain</strong></p>
+						</td>
+						<td >
+						<p>Couples</p>
+						</td>
+						<td >
+						<p>$160-$280/month</p>
+						</td>
+						<td >
+						<ul>
+						<li>Messaging</li>
+						<li>Live Video</li>
+						</ul>
+						</td>
+						<td >
+						<p>Matching</p>
+						</td>
+						<td >
+						<p>1,500+</p>
+						</td>
+						<td >
+						<p>8.5/10</p>
+						</td>
+						<td >
+						<p><span><a href="https://www.regain.us/">Link</a></span></p>
+						</td>
+						</tr>
+						<tr>
+						<td >
+						<p><strong>MDLive</strong></p>
+						</td>
+						<td >
+						<p>Live Session</p>
+						</td>
+						<td >
+						<p>$99-$259/session</p>
+						</td>
+						<td >
+						<ul>
+						<li>Phone Call</li>
+						<li>Live Video</li>
+						</ul>
+						</td>
+						<td >
+						<p>Selection</p>
+						</td>
+						<td >
+						<p>400+</p>
+						</td>
+						<td >
+						<p>7.9/10</p>
+						</td>
+						<td >
+						<p><span><a href="https://www.mdlive.com/counseling/">Link</a></span></p>
+						</td>
+						</tr>
+						<tr>
+						<td >
+						<p><strong>Amwell</strong></p>
+						</td>
+						<td >
+						<p>Psychiatry</p>
+						</td>
+						<td >
+						<p>$85-$99/session</p>
+						</td>
+						<td >
+						<ul>
+						<li>Phone Call</li>
+						<li>Live Video</li>
+						</ul>
+						</td>
+						<td >
+						<p>Selection</p>
+						</td>
+						<td >
+						<p>350+</p>
+						</td>
+						<td >
+						<p>7.7/10</p>
+						</td>
+						<td >
+						<p><span><a href="https://amwell.com/cm/services/online-psychiatry/">Link</a></span></p>
+						</td>
+						</tr>
+						<tr>
+						<td >
+						<p><strong>Teen Counseling</strong></p>
+						</td>
+						<td >
+						<p>Adolescents</p>
+						</td>
+						<td >
+						<p>$160-$280/month</p>
+						</td>
+						<td >
+						<ul>
+						<li>Messaging</li>
+						<li>Live Chat</li>
+						<li>Phone Call</li>
+						<li>Live Video</li>
+						</ul>
+						</td>
+						<td >
+						<p>Matching</p>
+						</td>
+						<td >
+						<p>3,500+</p>
+						</td>
+						<td >
+						<p>7.5/10</p>
+						</td>
+						<td >
+						<p><span><a href="https://www.teencounseling.com/">Link</a></span></p>
+						</td>
+						</tr>
+						<tr>
+						<td >
+						<p><strong>Pride Counseling</strong></p>
+						</td>
+						<td >
+						<p>LGBTQ</p>
+						</td>
+						<td >
+						<p>$160-$280/month</p>
+						</td>
+						<td >
+						<ul>
+						<li>Messaging</li>
+						<li>Live Chat</li>
+						<li>Phone Call</li>
+						<li>Live Video</li>
+						</ul>
+						</td>
+						<td >
+						<p>Matching</p>
+						</td>
+						<td >
+						<p>3,000+</p>
+						</td>
+						<td >
+						<p>7.5/10</p>
+						</td>
+						<td >
+						<p><span><a href="https://www.pridecounseling.com/">Link</a></span></p>
+						</td>
+						</tr>
+						<tr>
+						<td >
+						<p><strong>iCounseling</strong></p>
+						</td>
+						<td >
+						<p>Free Library</p>
+						</td>
+						<td >
+						<p>$160-$280/month</p>
+						</td>
+						<td >
+						<ul>
+						<li>Messaging</li>
+						<li>Live Chat</li>
+						<li>Phone Call</li>
+						<li>Live Video</li>
+						</ul>
+						</td>
+						<td >
+						<p>Matching</p>
+						</td>
+						<td >
+						<p>4,000+</p>
+						</td>
+						<td >
+						<p>6.9/10</p>
+						</td>
+						<td >
+						<p><span><a href="https://www.icounseling.com/">Link</a></span></p>
+						</td>
+						</tr>
+						<tr>
+						<td >
+						<p><strong>Doctor on Demand</strong></p>
+						</td>
+						<td >
+						<p>Free Assessment</p>
+						</td>
+						<td >
+						<p>$129-$299/session</p>
+						</td>
+						<td >
+						<ul>
+						<li>Phone Call</li>
+						<li>Live Video</li>
+						</ul>
+						</td>
+						<td >
+						<p>Selection</p>
+						</td>
+						<td >
+						<p>300+</p>
+						</td>
+						<td >
+						<p>7.3/10</p>
+						</td>
+						<td >
+						<p><span><a href="https://www.doctorondemand.com/what-we-treat/behavioral-health">Link</a></span></p>
+						</td>
+						</tr>
+						<tr>
+						<td >
+						<p><strong>PrestoExperts</strong></p>
+						</td>
+						<td >
+						<p>A la carte sessions</p>
+						</td>
+						<td >
+						<p>$1-$4.50/min</p>
+						</td>
+						<td >
+						<ul>
+						<li>Email</li>
+						<li>Live Chat</li>
+						<li>Phone Call</li>
+						</ul>
+						</td>
+						<td >
+						<p>Selection</p>
+						</td>
+						<td >
+						<p>42</p>
+						</td>
+						<td >
+						<p>7/10</p>
+						</td>
+						<td >
+						<p><span><a href="https://www.prestoexperts.com/professional-counseling/">Link</a></span></p>
+						</td>
+						</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+					
 					<div class="rs-block-content">
 					<h2>The Future of Telehealth</h2>
 					<p><span >The future of telehealth is exciting and promising. With more organizations and providers offering telemedicine and more patients in favor of utilizing telemedicine this form of medicine will continue to grow over time. In the future telemedicine could include international collaboration. If telemedicine could one day cross international borders than those countries that are more medically advanced could help breakdown medical barriers. Hopefully, the future of telehealth will include health system collaboration. Currently, it is difficult for two different health systems with different EMR (electronic medical records) systems to share electronic medical records information. Perhaps the future in telemedicine will allow for easier sharing of patient medical records which will allow providers to have a bigger picture of their patient’s medical history. All in all the future of telehealth is bright and it will be exciting to watch as it evolves over time.</span></p>
